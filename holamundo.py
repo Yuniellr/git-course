e44c5fa3631fbf8b1651a6bd873319d9c5fea842
Yuniellr <angelyuniel@gmail.com>
2021-07-20 19:37:46 -0700

    Update

--- holamundo.py
+++ holamundo.py
@@ -1,2 +1,13 @@
-print('Hi!, what you doing?')
-
+class Solution:
+    def addTwoNumbers(self,a,b):
+        self.a = print(input('hi insert a list'))
+        self.b = print(input('hi insert other list'))
+        
+        pass
+    
+    def reversa(self,l1):
+        self.l1 = self.a + self.b
+        list(reversed(self.l1))
+        pass
+        
+print(Solution)
